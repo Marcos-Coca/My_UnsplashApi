3279dd1086e8f9cca1addb82a2fb1e773042bddc
Marcos Mercado <marcoscoca0905@gmail.com>
2020-11-22 20:57:52 -0400

    Photo View Model

---
+++ Unsplash/Models/ViewModels/PhotoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Unsplash.Models.ViewModels
+{
+    public class PhotoViewModel
+    {
+        public int Id { get; set; }
+        public string Label { get; set; }
+        public string Url { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
--- Unsplash/Services/PhotoService.cs
+++ Unsplash/Services/PhotoService.cs
@@ -2,10 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Unsplash.Models;
 
 namespace Unsplash.Services
 {
-    public class PhotoService
+    public class PhotoService : IPhotoService
     {
+        public Photo GetPhoto(int photoId)
+        {
+            Photo photo;
+            using(UnsplashContext db =  new UnsplashContext())
+            {
+                photo = db.Photos select(new Photo)
+            }
+            return photo;
+        }
+        public List<Photo> GetAllPhotos(int userId)
+        {
+            throw new NotImplementedException();
+        }
+        public void DeletePhoto(int photoId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Photo> FindPhotos(int userId, string label)
+        {
+            throw new NotImplementedException();
+        }
+        
     }
 }
